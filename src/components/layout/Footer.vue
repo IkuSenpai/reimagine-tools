18158d2f7a69cb946c9baa414319917a79ed512c
IkuSenpai <dmorley1.dm@gmail.com>
2024-01-27 06:18:50 -0800

    Update Footer.vue
    Updated from the non-active original source to the new source.

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -3,8 +3,8 @@
 		<div class="content has-text-centered">
 			<p>
 				<strong>ReIMAGINE Calculators</strong> by
-				<a href="https://github.com/kuenaimaku">Kuenaimaku</a>. The <a href="https://github.com/Kuenaimaku/reimagine-tools">source code</a> is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.<br/>
-				I am not affiliated with the ReIMAGINE team. I just want to make planning for this game simpler.
+				<a href="https://github.com/IkuSenpai">IkuSenpai</a>. The <a href="https://github.com/IkuSenpai/reimagine-tools">source code</a> is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.<br/>
+				I am not affiliated with Atlus or Sega. I just want to make planning for this game simpler.
 			</p>
 		</div>
 	</footer>
