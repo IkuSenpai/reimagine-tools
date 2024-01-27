c352e9ac3917548950aabc4d12630fb0b889a482
IkuSenpai <dmorley1.dm@gmail.com>
2024-01-27 06:19:07 -0800

    Update Footer.vue

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -2,7 +2,7 @@
 	<footer class="footer">
 		<div class="content has-text-centered">
 			<p>
-				<strong>ReIMAGINE Calculators</strong> by
+				<strong>SMT Imagine Calculators</strong> by
 				<a href="https://github.com/IkuSenpai">IkuSenpai</a>. The <a href="https://github.com/IkuSenpai/reimagine-tools">source code</a> is licensed <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.<br/>
 				I am not affiliated with Atlus or Sega. I just want to make planning for this game simpler.
 			</p>
